92286f0de099f612602d464fe9bf30357ee77e49
Leo Hetterscheid <Testichon@gmail.com>
2018-12-16 14:21:48 +0100

    #6 - convertprofiles

--- Backend/src/main/java/database/convertProfiles.java
+++ Backend/src/main/java/database/convertProfiles.java
@@ -1,4 +1,17 @@
 package database;
 
+import Model.Profile;
+
+import java.sql.ResultSet;
+
 public class convertProfiles {
+    public static Profile createProfile(ResultSet rs) throws Exception {
+        Profile profile = new Profile();
+        profile.setId(rs.getString("id"));
+        profile.setName(rs.getNString("Name"));
+        profile.setEmail(rs.getString("Email"));
+        profile.setAdress(rs.getString("Adress"));
+        profile.setAge(rs.getInt("Age"));
+        return profile;
+    }
 }
